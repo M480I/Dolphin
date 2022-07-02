eae4f71a5b543c6677edbd9bcd2001c602505296
mohammad firouzi <98333787+mmd-firouzi@users.noreply.github.com>
2022-07-02 19:50:32 +0430

    Update Main.java
    add class person

--- Main.java
+++ Main.java
@@ -13,3 +13,29 @@ public class Main {
 	}
 	
 }
+
+
+//class person is supercalss of Teacehr and Student and Principal
+class Person{
+	
+	private String username;
+	private String password;
+	private String name;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+
+	
+}
